3a4130c21691b96edfbf6578b77c86a2de6d1da3
David Bayon <dbayon82@googlemail.com>
2014-10-21 14:16:21 +0100

    Solved prime factors

--- 3-largest-prime-factor/lib/prime_factor.rb
+++ 3-largest-prime-factor/lib/prime_factor.rb
@@ -1,3 +1,14 @@
-def largest_prime_factor_of(total)
+require 'prime'
 
+def largest_prime_factor_of(total)
+	factors = []
+	Prime.each(100000) do |prime|
+		if total % prime == 0
+			factors << prime
+			total = total / prime
+			return total if Prime.prime?(total)
+		end
+	end
 end
+
+puts largest_prime_factor_of(600851475143)
